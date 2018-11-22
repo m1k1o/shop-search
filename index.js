ca2dea223fcf7a893dd2b9229c3f07287efb0a28
Miroslav Šedivý <sedivy.miro@gmail.com>
2018-11-22 23:45:43 +0100

    big mistake with getOffer fixed

--- index.js
+++ index.js
@@ -20,38 +20,36 @@ let sortProducts = (shops, reverse = false) => {
     return offers
 }
 
-let getOffer = (shop, query) =>
-	(cb) => {
-			request(typeof shop.serp_url !== 'function' ? shop.serp_url+query : shop.serp_url(query), (e, { statusCode }, d) => {
-				if (statusCode !== 200) {
-					return cb(new Error(statusCode));
-				}
-
-				const $ = cheerio.load(d)
-
-				let result = [];
-				(typeof shop.item === 'function'
-					? shop.item
-					: ($, cb) => $(shop.item).each(cb)
-				)($, (i, e) => {
-					let elem = $(e)
-					result.push({
-						name: typeof shop.name !== 'function' ? elem.find(shop.name).text().trim() : shop.name(elem),
-						price: typeof shop.price !== 'function' ? parsePrice(elem.find(shop.price).text()) : shop.price(elem),
-						url: typeof shop.url !== 'function' ? elem.find(shop.url).attr('href') : shop.url(elem)
-					})
-				})
-
-				cb(e, result)
-			})
+let getOffer = (shop, query, cb) =>
+	request(typeof shop.serp_url !== 'function' ? shop.serp_url+query : shop.serp_url(query), (e, { statusCode }, d) => {
+		if (statusCode !== 200) {
+			return cb(new Error(statusCode));
 		}
+
+		const $ = cheerio.load(d)
+
+		let result = [];
+		(typeof shop.item === 'function'
+			? shop.item
+			: ($, cb) => $(shop.item).each(cb)
+		)($, (i, e) => {
+			let elem = $(e)
+			result.push({
+				name: typeof shop.name !== 'function' ? elem.find(shop.name).text().trim() : shop.name(elem),
+				price: typeof shop.price !== 'function' ? parsePrice(elem.find(shop.price).text()) : shop.price(elem),
+				url: typeof shop.url !== 'function' ? elem.find(shop.url).attr('href') : shop.url(elem)
+			})
+		})
+
+		cb(e, result)
+	})
 
 let getOffers = (shops, query, cb) =>
 	parallel(Array.isArray(shops)
-		? shops.map((shop) => getOffer(shop, query))
+		? shops.map((shop) => (cb) => getOffer(shop, query, cb))
 		: Object.assign(...Object.entries(shops).map(([k, shop]) =>
 			({
-				[k]: getOffer(shop, query)
+				[k]: (cb) => getOffer(shop, query, cb)
 			})
 		)), cb)
 
